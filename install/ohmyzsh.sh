d733fe2f71247b4d5e5ffd39e07e7748403bc828
malanore-z <malanore.z@gmail.com>
2021-10-17 19:42:43 +0800

    Fix bug

--- install/ohmyzsh.sh
+++ install/ohmyzsh.sh
@@ -1,12 +1,3 @@
 #! /bin/bash
 
-BASEDIR="$( cd "$( dirname "$0" )" && pwd )" # the directory of this file
-
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-cp ${BASEDIR}/malanore.zsh-theme $ZSH/themes
-
-sed -i 's/^ZSH_THEME=.*/ZSH_THEME="malanore"/g' ~/.zshrc
-sed -i 's/^plugins=.*/plugins=(git sudo virtualenv)/g' ~/.zshrc
-
-echo "" >> ~/.zshrc
-echo "source ~/.profile" >> ~/.zshrc
